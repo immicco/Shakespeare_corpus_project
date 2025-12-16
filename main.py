881a8c51fae0f3ed88ee3a15040f0aadc1a756b2
immicco <92093089+immicco@users.noreply.github.com>
2025-12-16 22:02:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,12 +47,101 @@ def analyze_corpus(corpus_folder, statistics_filepath):
         total_lines += a.get("lines_number")
         total_lexical_density += a.get("lexical_density")
     print(total_lines)
-    corpus_analysis["avg_ttr"] = total_ttr/total_number
-    corpus_analysis["avg_words_number"]= total_words/total_number
-    corpus_analysis["avg_lines"]= total_lines/total_number
-    corpus_analysis["avg_lexical_density"] = total_lexical_density/total_number
+    corpus_analysis["avg_ttr"] = round(total_ttr/total_number, 2)
+    corpus_analysis["avg_words_number"]= round(total_words/total_number, 2)
+    corpus_analysis["avg_lines"]= round(total_lines/total_number, 2)
+    corpus_analysis["avg_lexical_density"] = round(total_lexical_density/total_number, 2)
     return corpus_analysis
+
+def analyze_cycle(corpus_folder, statistics_filepath, metadata_filepath, cycle):
+    files_list = get_files_in_folder(corpus_folder)
+    analyses_list = []
+    statistics = []
+    headers = ["filename","number of words", "number of lines", "Type-Token Ratio", "lexical density"]
+    texts_to_analyze = {}
+    metadata = read_csvfile(metadata_filepath):
+    for line in metadata:
+      filename = line.get("filename", "")
+      if cycle == line.get("cycle", ""):
+        texts_to_analyze[filename] = True
+      else:
+        texts_to_analyze[filename] = False    
+    for f in files_list:
+        if texts_to_analyze[f] == True:
+          analysis = analyze_single_text(corpus_folder, f)
+          analyses_list.append(analysis)
+          statistics_line = [f, str(analysis.get("words_number", 0)), str(analysis.get("lines_number", 0)), str(analysis.get("ttr", 0)), str(analysis.get("lexical_density", 0))]
+          statistics.append(statistics_line)
+    write_csvfile(f"{statistics_filepath}/f"statistics_group_{group}.csv", statistics, headers)
+    corpus_analysis = {
+        "avg_ttr": 0,
+        "avg_words_number": 0,
+        "avg_lines": 0,
+        "avg_lexical_density": 0
+        }
+    total_number = len(analyses_list)
+    print(total_number)
+    total_ttr = 0
+    total_words = 0
+    total_lines = 0
+    total_lexical_density = 0
+    for a in analyses_list:
+        total_ttr += a.get("ttr")
+        total_words += a.get("words_number")
+        total_lines += a.get("lines_number")
+        total_lexical_density += a.get("lexical_density")
+    print(total_lines)
+    corpus_analysis["avg_ttr"] = round(total_ttr/total_number, 2)
+    corpus_analysis["avg_words_number"]= round(total_words/total_number, 2)
+    corpus_analysis["avg_lines"]= round(total_lines/total_number, 2)
+    corpus_analysis["avg_lexical_density"] = round(total_lexical_density/total_number, 2)
+    return corpus_analysis
+    
+  def analyze_group(corpus_folder, statistics_filepath, metadata_filepath, group):
+    files_list = get_files_in_folder(corpus_folder)
+    analyses_list = []
+    statistics = []
+    headers = ["filename","number of words", "number of lines", "Type-Token Ratio", "lexical density"]
+    texts_to_analyze = {}
+    metadata = read_csvfile(metadata_filepath):
+    for line in metadata:
+      filename = line.get(filename, "")
+      if group == line.get(group, ""):
+        texts_to_analyze[filename] = True
+      else:
+        texts_to_analyze[filename] = False
         
+    for f in files_list:
+        if texts_to_analyze[f] == True:
+          analysis = analyze_single_text(corpus_folder, f)
+          analyses_list.append(analysis)
+          statistics_line = [f, str(analysis.get("words_number", 0)), str(analysis.get("lines_number", 0)), str(analysis.get("ttr", 0)), str(analysis.get("lexical_density", 0))]
+          statistics.append(statistics_line)
+    write_csvfile(f"{statistics_filepath}/f"statistics_group_{group}.csv", statistics, headers)
+    corpus_analysis = {
+        "avg_ttr": 0,
+        "avg_words_number": 0,
+        "avg_lines": 0,
+        "avg_lexical_density": 0
+        }
+    total_number = len(analyses_list)
+    print(total_number)
+    total_ttr = 0
+    total_words = 0
+    total_lines = 0
+    total_lexical_density = 0
+    for a in analyses_list:
+        total_ttr += a.get("ttr")
+        total_words += a.get("words_number")
+        total_lines += a.get("lines_number")
+        total_lexical_density += a.get("lexical_density")
+    print(total_lines)
+    corpus_analysis["avg_ttr"] = round(total_ttr/total_number, 2)
+    corpus_analysis["avg_words_number"]= round(total_words/total_number, 2)
+    corpus_analysis["avg_lines"]= round(total_lines/total_number, 2)
+    corpus_analysis["avg_lexical_density"] = round(total_lexical_density/total_number, 2)
+    return corpus_analysis
+  
 if __name__ == "__main__":
   sample_analysis = analyze_single_text("corpus", "126.txt")
 
