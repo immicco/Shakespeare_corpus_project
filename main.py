f64644e02a8cd7af3363a997288855bb3e572a57
immicco <92093089+immicco@users.noreply.github.com>
2025-12-16 21:38:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from file_utils import file_utils.py
+from text_utils import text_utils.py
+
+def analyze_single_text(filename):
+  text = read_txtfile(f"corpus/{filename}")
+  words_number = count_words(text)
+  unique_words_number = count_unique_words(text)
+  lines_number = count_lines(text)
+  ttr = calculate_ttr(text)
+  lexical_density = calculate_lexical_density(text)
+  avg_word_length = average_word_length(text)
+  longest_word = find_longest_word(text)
 
